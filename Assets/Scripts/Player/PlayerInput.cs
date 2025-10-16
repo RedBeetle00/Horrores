aa0b9fbd25728bd7962fcee7cb5b0b841952fe3b
RedBeetle <redjuks@gmail.com>
2025-10-16 03:38:22 +0300

    Move left-right

---
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInput : MonoBehaviour
+{
+    // Создаём переменные для ввода игрока из InputSystem
+    public static InputAction playerInputMove;
+    public static InputAction playerInputJump;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        // Получаем ввод игрока
+        playerInputMove = InputSystem.actions.FindAction("Move");
+        playerInputJump = InputSystem.actions.FindAction("Jump");
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    private Rigidbody2D rb; // Переменная для физического тела
+    private PlayerInput playerInput; // Переменная для передачи кода из одного файла в другой
+    
+    // SerializeField означает, что несмотря на private, переменную можно будет менять из инспектора юнити
+    [SerializeField] private float playerSpeed; 
+    [SerializeField] private float jumpStrengh;
+
+    public Vector2 moveAmt; // Переменная для хранения вектора движения игрока
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        playerInput = GetComponent<PlayerInput>(); // Получаем код из файла для ввода
+        rb = GetComponent<Rigidbody2D>(); // Получаем Rigidbody
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveAmt = PlayerInput.playerInputMove.ReadValue<Vector2>(); // Получаем вектор движения игрока из скрипта PlayerInput
+
+        if (PlayerInput.playerInputJump.WasPressedThisFrame()) // Если пробел был нажат, то...
+        {
+            Jump();
+        }
+    }
+
+    // Обновляется каждые 50 (вроде, так сказал дипсик) кадров
+    private void FixedUpdate()
+    {
+        Walking();
+    }
+
+    private void Walking()
+    {
+        rb.MovePosition(rb.position + Vector2.right * moveAmt.x * playerSpeed * Time.deltaTime); // Берём позицию игрока и прибавляем к ней вектор движения умноженный на скорость. Time.deltaTime делает так, что частота кадров не будет влиять на скорость перемещения
+    }
+
+    private void Jump()
+    {
+        rb.AddForce(Vector2.up * jumpStrengh, ForceMode2D.Impulse); // Пытаюсь сделать прыжок, но эта хенря не работает
+        Debug.Log("Jump"); // Выводит в консоль сообщение. Использую для проверки на то, что метов вообще срабатывает
+    }
+}
